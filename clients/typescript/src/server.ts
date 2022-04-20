484a6e949a9c64e07114727cccb33f10a7173c11
Adrien Saunier <asaunier@octo.com>
2022-04-21 01:20:35 +0200

    Met à jour la configuration TS

--- clients/typescript/src/server.ts
+++ clients/typescript/src/server.ts
@@ -21,7 +21,8 @@ router.post('/order', async (ctx) => {
 
 app.use(router.routes());
 
-app.listen(8080);
+const port = process.env.PORT || '8080';
+app.listen(port);
 
-console.log('Server running on port 8080');
+console.log(`Server running on port ${port}`);
 
